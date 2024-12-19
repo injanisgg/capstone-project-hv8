72655bbff321bdb2d77493e0444db80df7bad207
Sugini Injani <forggdev@gmail.com>
2024-12-19 14:39:51 +0700

    `Update h1 element in DressStyle component with responsive design classes`

--- src/components/DressStyle.jsx
+++ src/components/DressStyle.jsx
@@ -9,7 +9,7 @@ function DressStyle() {
   return (
     <>
     <div className='bg-landing mx-20 my-16 rounded-3xl h-[800px] w-max-2xl grid grid-cols-12 grid-rows-12 gap-4'>
-        <h1 className='text-main-army font-extrabold text-4xl leading-tight col-start-4 col-span-6 row-start-2 ml-[82px] md:col-start-4 md:col-span-6 md:ml-0'>EXPLORE DRESSES BY DESIGN</h1>
+        <h1 className='text-main-army font-extrabold text-4xl leading-tight 1xl:col-start-4 1xl:col-span-6 1xl:row-start-2 1xl:ml-[82px] xl:col-start-4 xl:col-span-6 xl:row-start-2 xl:ml-4'>EXPLORE DRESSES BY DESIGN</h1>
         <div className='col-start-2 col-span-4 row-start-4 row-span-3'>
             <CardRelative picture={Casual} title={'Casual'} alt={'Casual'}/>
         </div>
